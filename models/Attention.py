74a4d2f61787608235eeed040be1b14bd3e92731
Vision <gitesh.ch.0912@gmail.com>
2023-03-26 00:55:46 +0530

    update

--- models/Attention.py
+++ models/Attention.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow.keras import layers
 from typing import List, Union
 import numpy as np
 
@@ -13,50 +14,49 @@ def shape_list(tensor: Union[tf.Tensor, np.ndarray]) -> List[int]:
     static = tensor.shape.as_list()
     return [dynamic[i] if s is None else s for i, s in enumerate(static)]
 
-
 class Attention(tf.keras.layers.Layer):
     def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0., sr_ratio=1):
-        super().__init__()
-        assert dim % num_heads == 0, f"dim {dim} should be divided by num_heads {num_heads}."
+        super(Attention, self).__init__()
+        assert dim % num_heads == 0, f"dim {dim} should be divisible by num_heads {num_heads}."
 
         self.dim = dim
         self.num_heads = num_heads
         head_dim = dim // num_heads
         self.scale = qk_scale or head_dim ** -0.5
 
-        self.q = tf.keras.layers.Dense(dim, use_bias=qkv_bias)
-        self.kv = tf.keras.layers.Dense(dim * 2, use_bias=qkv_bias)
-        self.attn_drop = tf.keras.layers.Dropout(attn_drop)
-        self.proj = tf.keras.layers.Dense(dim)
-        self.proj_drop = tf.keras.layers.Dropout(proj_drop)
+        self.q = layers.Dense(dim, use_bias=qkv_bias)
+        self.kv = layers.Dense(dim * 2, use_bias=qkv_bias)
+        self.attn_drop = layers.Dropout(attn_drop)
+        self.proj = layers.Dense(dim)
+        self.proj_drop = layers.Dropout(proj_drop)
 
         self.sr_ratio = sr_ratio
         if sr_ratio > 1:
-            self.sr = tf.keras.layers.Conv2D(dim, kernel_size=sr_ratio, strides=sr_ratio)
-            self.norm = tf.keras.layers.LayerNormalization()
+            self.sr = layers.Conv2D(dim, kernel_size=sr_ratio, strides=sr_ratio)
+            self.norm = layers.LayerNormalization()
 
     def call(self, x, H, W):
-        B, N, C = x.shape
+        B = shape_list(x)[0]
+        N = shape_list(x)[1]
+        C = shape_list(x)[2]        
         q = self.q(x)
         q = tf.reshape(q, (B, N, self.num_heads, C // self.num_heads))
         q = tf.transpose(q, perm=[0, 2, 1, 3])
-
         if self.sr_ratio > 1:
             x_ = tf.transpose(x, perm=[0, 2, 1])
-            x_ = tf.reshape(x_, (B, C, H, W))
+            x_ = tf.reshape(x_, (B, H, W, C))
             x_ = self.sr(x_)
-            x_ = tf.reshape(x_, (B, C, -1))
-            x_ = tf.transpose(x_, perm=[0, 2, 1])
+            x_ = tf.reshape(x_, (B, shape_list(x_)[1]*shape_list(x_)[2], C))
             x_ = self.norm(x_)
             kv = self.kv(x_)
-            kv = tf.reshape(kv, (B, -1, 2, self.num_heads, C // self.num_heads))
+            kv = tf.reshape(kv, (B, shape_list(kv)[1], 2, self.num_heads, C // self.num_heads))
             kv = tf.transpose(kv, perm=[2, 0, 3, 1, 4])
         else:
             kv = self.kv(x)
-            kv = tf.reshape(kv, (B, -1, 2, self.num_heads, C // self.num_heads))
+            kv = tf.reshape(kv, (B, shape_list(kv)[1], 2, self.num_heads, C // self.num_heads))
             kv = tf.transpose(kv, perm=[2, 0, 3, 1, 4])
-        k, v = kv[:, 0], kv[:, 1]
 
+        k, v = kv[0], kv[1]
         attn = tf.matmul(q, tf.transpose(k, perm=[0, 1, 3, 2])) * self.scale
         attn = tf.nn.softmax(attn, axis=-1)
         attn = self.attn_drop(attn)
@@ -66,5 +66,4 @@ class Attention(tf.keras.layers.Layer):
         x = tf.reshape(x, (B, N, C))
         x = self.proj(x)
         x = self.proj_drop(x)
-
         return x
--- models/modules.py
+++ models/modules.py
@@ -17,7 +17,7 @@ def shape_list(tensor: Union[tf.Tensor, np.ndarray]) -> List[int]:
     return [dynamic[i] if s is None else s for i, s in enumerate(static)]
 
 
-class DropPath(tf.keras.layers.Layer):
+class DropPath(layers.Layer):
     def __init__(self, drop_path, **kwargs):
         super().__init__(**kwargs)
         self.drop_path = drop_path
@@ -32,98 +32,117 @@ class DropPath(tf.keras.layers.Layer):
         return x
 
 
-class DWConv(tf.keras.layers.Layer):
-    def __init__(self, dim: int = 768, **kwargs):
+class DWConv(layers.Layer):
+    def __init__(self, filters=768, **kwargs):
         super().__init__(**kwargs)
-        self.depthwise_convolution = tf.keras.layers.Conv2D(
-            filters=dim,
+        self.dwconv = tf.keras.layers.Conv2D(
+            filters=filters,
             kernel_size=3,
             strides=1,
             padding="same",
-            groups=dim,
+            groups=filters,
         )
 
-    def call(self, x: tf.Tensor, height: int, width: int) -> tf.Tensor:
-        batch_size = shape_list(x)[0]
-        num_channels = shape_list(x)[-1]
-        x = tf.reshape(x, (batch_size, height, width, num_channels))
-        x = self.depthwise_convolution(x)
-
-        new_height = shape_list(x)[1]
-        new_width = shape_list(x)[2]
-        num_channels = shape_list(x)[3]
-        x = tf.reshape(x, (batch_size, new_height * new_width, num_channels))
+    def call(self, x, H, W):
+        B = shape_list(x)[0]
+        C = shape_list(x)[-1]
+        x = tf.reshape(x, (B, H, W, C))
+        x = self.dwconv(x)
+        NH = shape_list(x)[1]
+        NW = shape_list(x)[2]
+        C = shape_list(x)[3]
+        x = tf.reshape(x, (B, NH * NW, C))
         return x
 
 
-class mlp(tf.keras.layers.Layer):
-    def __init__(self, dim, dropout):
+class Mlp(layers.Layer):
+    def __init__(
+        self,
+        in_features,
+        hidden_features=None,
+        out_features=None,
+        drop=0.0,
+    ):
         super().__init__()
-        self.fc1 = tf.keras.layers.Dense(
-            dim, kernel_initializer=tf.keras.initializers.GlorotNormal()
-        )
-        self.dwconv = DWConv(dim)
-        self.fc2 = tf.keras.layers.Dense(
-            dim, kernel_initializer=tf.keras.initializers.GlorotNormal()
-        )
-        self.drop = tf.keras.layers.Dropout(dropout)
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = layers.Dense(hidden_features)
+        self.dwconv = DWConv(hidden_features)
+        self.act = layers.Activation("gelu")
+        self.fc2 = layers.Dense(out_features)
+        self.drop = layers.Dropout(drop)
 
-    def call(self, input_tensor, H, W):
-        x = self.fc1(input_tensor)
+    def call(self, x, H, W):
+        x = self.fc1(x)
         x = self.dwconv(x, H, W)
-        x = tf.nn.gelu(x)
+        x = self.act(x)
         x = self.drop(x)
         x = self.fc2(x)
         x = self.drop(x)
         return x
 
 
-class Block(tf.keras.layers.Layer):
+class Block(layers.Layer):
     def __init__(
         self,
         dim,
         num_heads,
         mlp_ratio=4.0,
-        qkv_bias=False,
-        qk_scale=None,
         drop=0.0,
         attn_drop=0.0,
         drop_path=0.0,
         sr_ratio=1,
     ):
         super().__init__()
-        self.norm1 = tf.keras.layers.LayerNormalization()
-        self.Attention = Attention(
-            dim, num_heads, sr_ratio, attn_drop
+        self.norm1 = layers.LayerNormalization(epsilon=1e-05)
+        self.attn = Attention(
+            dim,
+            num_heads=num_heads,
+            attn_drop=attn_drop,
+            proj_drop=drop,
+            sr_ratio=sr_ratio,
         )
-        self.drop_path = DropPath(drop_path)
-        self.mlp = mlp(dim * mlp_ratio, drop)
-        self.norm2 = tf.keras.layers.LayerNormalization()
+        self.drop_path = (
+            DropPath(drop_path) if drop_path > 0.0 else tf.keras.layers.Layer()
+        )
+        self.norm2 = layers.LayerNormalization(epsilon=1e-05)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(
+            in_features=dim,
+            hidden_features=mlp_hidden_dim,
+            drop=drop,
+        )
 
     def call(self, x, H, W):
-        x = x + self.drop_path(self.Attention(self.norm1(x), H, W))
+        x = x + self.drop_path(self.attn(self.norm1(x), H, W))
         x = x + self.drop_path(self.mlp(self.norm2(x), H, W))
         return x
 
-class OverlapPatchEmbed(tf.keras.Model):
-    def __init__(self, img_size=224, patch_size=7, stride=4, embed_dim=768, **kwargs):
+
+class OverlapPatchEmbed(layers.Layer):
+    def __init__(
+        self, img_size=224, patch_size=7, stride=4, filters=768, **kwargs
+    ):
         super().__init__(**kwargs)
-        self.padding = tf.keras.layers.ZeroPadding2D(padding=patch_size // 2)
-        self.conv = tf.keras.layers.Conv2D(
-            filters=embed_dim, kernel_size=patch_size, strides=stride, padding="VALID")
-        self.norm = tf.keras.layers.LayerNormalization(epsilon=1e-05)
+        self.conv = layers.Conv2D(
+            filters=filters,
+            kernel_size=patch_size,
+            strides=stride,
+            padding="same",
+        )
+        self.norm = layers.LayerNormalization(epsilon=1e-05)
 
     def call(self, x):
-        x = self.conv(self.padding(x))
+        x = self.conv(x)
         H = shape_list(x)[1]
         W = shape_list(x)[2]
-        N = shape_list(x)[3]
-
-        x = tf.reshape(x, (-1, H * W, N))
+        x = tf.reshape(x, (-1, H * W, shape_list(x)[3]))
         x = self.norm(x)
+        print(x.shape)
         return x, H, W
 
-class MixVisionTransformer(tf.keras.layers.Layer):
+
+class MixVisionTransformer(layers.Layer):
     def __init__(
         self,
         img_size=224,
@@ -133,8 +152,6 @@ class MixVisionTransformer(tf.keras.layers.Layer):
         embed_dims=[64, 128, 256, 512],
         num_heads=[1, 2, 4, 8],
         mlp_ratios=[4, 4, 4, 4],
-        qkv_bias=False,
-        qk_scale=None,
         drop_rate=0.0,
         attn_drop_rate=0.0,
         drop_path_rate=0.0,
@@ -145,101 +162,153 @@ class MixVisionTransformer(tf.keras.layers.Layer):
         self.num_classes = num_classes
         self.depths = depths
 
-        #patch_embed
-        self.patch_embed1 = OverlapPatchEmbed(img_size=img_size, patch_size=7, stride=4,
-                                              embed_dim=embed_dims[0])
-        self.patch_embed2 = OverlapPatchEmbed(img_size=img_size // 4, patch_size=3, stride=2,
-                                              embed_dim=embed_dims[1])
-        self.patch_embed3 = OverlapPatchEmbed(img_size=img_size // 8, patch_size=3, stride=2,
-                                              embed_dim=embed_dims[2])
-        self.patch_embed4 = OverlapPatchEmbed(img_size=img_size // 16, patch_size=3, stride=2,
-                                              embed_dim=embed_dims[3])
+        # patch_embed
+        self.patch_embed1 = OverlapPatchEmbed(
+            img_size=img_size,
+            patch_size=7,
+            stride=4,
+            filters=embed_dims[0],
+        )
+        self.patch_embed2 = OverlapPatchEmbed(
+            img_size=img_size // 4,
+            patch_size=3,
+            stride=2,
+            filters=embed_dims[1],
+        )
+        self.patch_embed3 = OverlapPatchEmbed(
+            img_size=img_size // 8,
+            patch_size=3,
+            stride=2,
+            filters=embed_dims[2],
+        )
+        self.patch_embed4 = OverlapPatchEmbed(
+            img_size=img_size // 16,
+            patch_size=3,
+            stride=2,
+            filters=embed_dims[3],
+        )
 
-        dpr = [x.numpy() for x in tf.linspace(0.0, drop_path_rate, sum(depths))]
+        # transformer encoder
+        dpr = [
+            x for x in tf.linspace(0.0, drop_path_rate, sum(depths))
+        ]  # stochastic depth decay rule
         cur = 0
-        self.block1 = []
-        for i in range(depths[0]):
-            new_block1 = Block(
-            dim=embed_dims[0], num_heads=num_heads[0], mlp_ratio=mlp_ratios[0], qkv_bias=qkv_bias, qk_scale=qk_scale,
-            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[cur + i],
-            sr_ratio=sr_ratios[0])
-            self.block1.append(new_block1)
-        self.norm1 = tf.keras.layers.LayerNormalization()
+        self.block1 = [
+            Block(
+                dim=embed_dims[0],
+                num_heads=num_heads[0],
+                mlp_ratio=mlp_ratios[0],
+                drop=drop_rate,
+                attn_drop=attn_drop_rate,
+                drop_path=dpr[cur + i],
+                sr_ratio=sr_ratios[0],
+            )
+            for i in range(depths[0])
+        ]
+        self.norm1 = layers.LayerNormalization(epsilon=1e-05)
 
         cur += depths[0]
-        self.block2 = []
-        for i in range(depths[1]):
-            new_block2 = Block(
-            dim=embed_dims[1], num_heads=num_heads[1], mlp_ratio=mlp_ratios[1], qkv_bias=qkv_bias, qk_scale=qk_scale,
-            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[cur + i],
-            sr_ratio=sr_ratios[1])
-            self.block2.append(new_block2)
-        self.norm2 = tf.keras.layers.LayerNormalization()
+        self.block2 = [
+            Block(
+                dim=embed_dims[1],
+                num_heads=num_heads[1],
+                mlp_ratio=mlp_ratios[1],
+                drop=drop_rate,
+                attn_drop=attn_drop_rate,
+                drop_path=dpr[cur + i],
+                sr_ratio=sr_ratios[1],
+            )
+            for i in range(depths[1])
+        ]
+        self.norm2 = layers.LayerNormalization(epsilon=1e-05)
 
         cur += depths[1]
-        self.block3 = []
-        for i in range(depths[2]):
-            new_block3 = Block(
-            dim=embed_dims[2], num_heads=num_heads[2], mlp_ratio=mlp_ratios[2], qkv_bias=qkv_bias, qk_scale=qk_scale,
-            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[cur + i],
-            sr_ratio=sr_ratios[2])
-            self.block3.append(new_block3)
-        self.norm3 = tf.keras.layers.LayerNormalization()
+        self.block3 = [
+            Block(
+                dim=embed_dims[2],
+                num_heads=num_heads[2],
+                mlp_ratio=mlp_ratios[2],
+                drop=drop_rate,
+                attn_drop=attn_drop_rate,
+                drop_path=dpr[cur + i],
+                sr_ratio=sr_ratios[2],
+            )
+            for i in range(depths[2])
+        ]
+        self.norm3 = layers.LayerNormalization(epsilon=1e-05)
 
         cur += depths[2]
-        self.block4 = []
-        for i in range(depths[3]):
-            new_block4 = Block(
-            dim=embed_dims[3], num_heads=num_heads[3], mlp_ratio=mlp_ratios[3], qkv_bias=qkv_bias, qk_scale=qk_scale,
-            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[cur + i],
-            sr_ratio=sr_ratios[3])
-            self.block4.append(new_block4)
-        self.norm4 = tf.keras.layers.LayerNormalization()
+        self.block4 = [
+            Block(
+                dim=embed_dims[3],
+                num_heads=num_heads[3],
+                mlp_ratio=mlp_ratios[3],
+                drop=drop_rate,
+                attn_drop=attn_drop_rate,
+                drop_path=dpr[cur + i],
+                sr_ratio=sr_ratios[3],
+            )
+            for i in range(depths[3])
+        ]
+        self.norm4 = layers.LayerNormalization(epsilon=1e-05)
 
-    def call_feature(self, x):
-        batch = x.shape[0]
+    def call_features(self, x):
+        B = tf.shape(x)[0]
         outs = []
+
+        # stage 1
         x, H, W = self.patch_embed1(x)
         for i, blk in enumerate(self.block1):
             x = blk(x, H, W)
         x = self.norm1(x)
-        height = width = int(x.shape[1] ** 0.5)
-        x = tf.reshape(x, (batch, height, width, -1))
+        x = tf.reshape(x, (B, H, W, -1))
+        x = tf.transpose(x, perm=[0, 3, 1, 2])
         outs.append(x)
 
+        # stage 2
         x, H, W = self.patch_embed2(x)
         for i, blk in enumerate(self.block2):
             x = blk(x, H, W)
         x = self.norm2(x)
-        height = width = int(x.shape[1] ** 0.5)
-        x = tf.reshape(x, (batch, height, width, -1))
+        x = tf.reshape(x, (B, H, W, -1))
+        x = tf.transpose(x, perm=[0, 3, 1, 2])
         outs.append(x)
 
+        # stage 3
         x, H, W = self.patch_embed3(x)
         for i, blk in enumerate(self.block3):
             x = blk(x, H, W)
         x = self.norm3(x)
-        height = width = int(x.shape[1] ** 0.5)
-        x = tf.reshape(x, (batch, height, width, -1))
+        x = tf.reshape(x, (B, H, W, -1))
+        x = tf.transpose(x, perm=[0, 3, 1, 2])
         outs.append(x)
 
+        # stage 4
         x, H, W = self.patch_embed4(x)
         for i, blk in enumerate(self.block4):
             x = blk(x, H, W)
         x = self.norm4(x)
-        height = width = int(x.shape[1] ** 0.5)
-        x = tf.reshape(x, (batch, height, width, -1))
+        x = tf.reshape(x, (B, H, W, -1))
+        x = tf.transpose(x, perm=[0, 3, 1, 2])
         outs.append(x)
 
         return outs
 
     def call(self, x):
-        x = self.call_feature(x)
+        x = self.call_features(x)
         return x
 
 
 input_layer = tf.keras.layers.Input(shape=(224, 224, 3))
-x = MixVisionTransformer(img_size = 224, patch_size=4, embed_dims=[32, 64, 160, 256], num_heads=[1, 2, 5, 8], mlp_ratios=[4, 4, 4, 4],
-            qkv_bias=True, depths=[2, 2, 2, 2], sr_ratios=[8, 4, 2, 1],
-            drop_rate=0.0, drop_path_rate=0.1)(input_layer)
+x = MixVisionTransformer(
+    img_size=224,
+    patch_size=4,
+    embed_dims=[32, 64, 160, 256],
+    num_heads=[1, 2, 5, 8],
+    mlp_ratios=[4, 4, 4, 4],
+    depths=[2, 2, 2, 2],
+    sr_ratios=[8, 4, 2, 1],
+    drop_rate=0.0,
+    drop_path_rate=0.1,
+)(input_layer)
 model = tf.keras.Model(inputs=input_layer, outputs=x)
